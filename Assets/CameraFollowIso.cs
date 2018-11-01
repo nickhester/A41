653af5ddd0d39f8f5ca8a891211a51991074b6c0
Nick Hester <nick.d.hester@accruent.com>
2018-11-01 00:10:07 -0500

    first commit

---
+++ Assets/CameraFollowIso.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowIso : MonoBehaviour {
+
+    public GameObject followTarget;
+
+    private Vector3 targetToCameraOffset;
+
+    private Transform targetTrans;
+    private Transform cameraTrans;
+
+    // Use this for initialization
+    void Start () {
+        targetTrans = followTarget.GetComponent<Transform>();
+        cameraTrans = transform;
+
+        targetToCameraOffset = targetTrans.position - cameraTrans.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        cameraTrans.SetPositionAndRotation(targetTrans.position - targetToCameraOffset, cameraTrans.rotation);
+	}
+}
---
+++ Assets/Stylized Astronaut/Character/Player.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    // ==========================================
+    
+    
+    
+    public float variableA = 7.0f;
+	public float variableB = 150.0f;
+	public float variableC = 20.0f;
+    public float variableD = 1.0f;
+    public float variableE = 10.0f;
+    
+
+
+    // ==========================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	private Animator anim;
+	private CharacterController controller;
+	private Vector3 moveDirection = Vector3.zero;
+    private bool isJumpingUp = false;
+    private float jumpHeight = 0.5f;
+    private float jumpStartHeight = 0.0f;
+
+	void Start () {
+		controller = GetComponent <CharacterController>();
+		anim = gameObject.GetComponentInChildren<Animator>();
+
+    }
+
+	void Update (){
+        transform.localScale = new Vector3(variableE, variableE, variableE);
+
+        if (Input.GetAxis("Vertical") > 0) {
+			anim.SetInteger ("AnimationPar", 1);
+		}  else {
+			anim.SetInteger ("AnimationPar", 0);
+		}
+        
+		if(controller.isGrounded){
+			moveDirection = transform.forward * Input.GetAxis("Vertical") * variableA;
+
+            if (Input.GetAxis("Jump") > 0.0f)
+            {
+                isJumpingUp = true;
+                jumpStartHeight = transform.position.y;
+            }
+        }
+        
+
+        if (isJumpingUp)
+        {
+            if (transform.position.y < jumpHeight + jumpStartHeight)
+            {
+                moveDirection += transform.up * variableD;
+            }
+            else
+            {
+                isJumpingUp = false;
+            }
+        }
+
+		float turn = Input.GetAxis("Horizontal");
+		transform.Rotate(0, turn * variableB * Time.deltaTime, 0);
+		controller.Move(moveDirection * Time.deltaTime);
+		moveDirection.y -= variableC * Time.deltaTime;
+	}
+}
