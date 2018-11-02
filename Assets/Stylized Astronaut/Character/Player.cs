e7abb64ae270212415b70211b5cb205c8add18fd
Nick Hester <nick.d.hester@accruent.com>
2018-11-01 21:38:39 -0500

    cleaning up formatting

--- Assets/Stylized Astronaut/Character/Player.cs
+++ Assets/Stylized Astronaut/Character/Player.cs
@@ -6,55 +6,45 @@ public class Player : MonoBehaviour
     // ==========================================
     
     
-    
     public float variableA = 7.0f;
-	public float variableB = 150.0f;
-	public float variableC = 20.0f;
+    public float variableB = 150.0f;
+    public float variableC = 20.0f;
     public float variableD = 1.0f;
     public float variableE = 10.0f;
-    
 
 
     // ==========================================
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-	private Animator anim;
-	private CharacterController controller;
-	private Vector3 moveDirection = Vector3.zero;
+    private Animator anim;
+    private CharacterController controller;
+    private Vector3 moveDirection = Vector3.zero;
     private bool isJumpingUp = false;
     private float jumpHeight = 0.5f;
     private float jumpStartHeight = 0.0f;
 
-	void Start () {
-		controller = GetComponent <CharacterController>();
-		anim = gameObject.GetComponentInChildren<Animator>();
-
+    void Start ()
+    {
+        controller = GetComponent <CharacterController>();
+        anim = gameObject.GetComponentInChildren<Animator>();
     }
 
-	void Update (){
+    void Update ()
+    {
         transform.localScale = new Vector3(variableE, variableE, variableE);
 
-        if (Input.GetAxis("Vertical") > 0) {
-			anim.SetInteger ("AnimationPar", 1);
-		}  else {
-			anim.SetInteger ("AnimationPar", 0);
-		}
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            anim.SetInteger ("AnimationPar", 1);
+        }
+        else
+        {
+            anim.SetInteger ("AnimationPar", 0);
+        }
         
-		if(controller.isGrounded){
-			moveDirection = transform.forward * Input.GetAxis("Vertical") * variableA;
+        if(controller.isGrounded)
+        {
+            moveDirection = transform.forward * Input.GetAxis("Vertical") * variableA;
 
             if (Input.GetAxis("Jump") > 0.0f)
             {
@@ -76,9 +66,9 @@ public class Player : MonoBehaviour
             }
         }
 
-		float turn = Input.GetAxis("Horizontal");
-		transform.Rotate(0, turn * variableB * Time.deltaTime, 0);
-		controller.Move(moveDirection * Time.deltaTime);
-		moveDirection.y -= variableC * Time.deltaTime;
-	}
+        float turn = Input.GetAxis("Horizontal");
+        transform.Rotate(0, turn * variableB * Time.deltaTime, 0);
+        controller.Move(moveDirection * Time.deltaTime);
+        moveDirection.y -= variableC * Time.deltaTime;
+    }
 }
